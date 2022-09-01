fb7e2a25c5f76fec39b5640b3089d351a0d7af5a
Davide <davide@test.com>
2022-09-01 23:05:50 +0200

    Lot of stuffs

--- src/app/core/services/db/db.service.ts
+++ src/app/core/services/db/db.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import * as lowdb from 'lowdb';
-import { org_model } from '../../../store/orgs/model';
+import { org_model, profile_model } from '../../../store/orgs/model';
 import * as fs from 'fs';
+import { Guid } from 'guid-typescript';
 
 @Injectable({
   providedIn: 'root',
@@ -18,7 +19,7 @@ export class DbService {
     const dir: string = ipc.sendSync('getHomeDir');
     if (!this.fs.existsSync(dir)) {
       this.fs.mkdir(dir, { recursive: true }, (err) => {
-        if (err) {throw err;}
+        if (err) { throw err; }
       });
     }
 
@@ -29,10 +30,53 @@ export class DbService {
   }
 
   getOrgs(): org_model[] {
-    return this.db.get('orgs').value();
+    const version = this.db.get('version').value();
+
+    const orgs = this.db.get('orgs').value() as org_model[];
+
+    // Migration
+    if (version === undefined) {
+      for (const org of orgs) {
+        const adminProfile = org.profiles.find(p => (p as any).innerName === (org as any).admin ||
+          p.name === (org as any).admin);
+
+        if (adminProfile !== undefined) {
+          org.profiles = org.profiles.filter(p => (p as any).innerName !== (org as any).admin &&
+            p.name !== (org as any).admin);
+
+          org.administrator = {
+            login: adminProfile.login,
+            pwd: adminProfile.pwd
+          };
+        }
+
+        // remove innername
+        org.profiles =  org.profiles.map(prof => {
+          const copy: profile_model = {
+            name: prof.name,
+            login: prof.login,
+            pwd: prof.pwd,
+            loginType: prof.loginType ?? 'Standard'
+          };
+          return copy;
+        });
+
+        if (org.id === null || org.id === undefined) {
+          org.id = Guid.create().toString();
+        }
+
+        // remove old Admin data
+        delete (org as any).admin;
+      }
+
+      this.save(orgs);
+    }
+
+    return orgs;
   }
 
   save(orgs: org_model[]): void {
+    this.db.set('version', 2).write();
     this.db.set('orgs', orgs).write();
   }
 }
